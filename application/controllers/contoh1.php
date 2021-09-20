a8f4b21e525aba392477e687e545467469c52104
nannisa727@gmail.com <nannisa727@gmail.com>
2021-09-20 16:12:49 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Annisa Nurul Madinah
+ Saya tingga di daerah Sintang
+ olah raga yang saya sukai adalah
+ Berenang";
+ }
+}
