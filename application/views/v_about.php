a2c8d074f6256af73176967ce8db1da0febe7a38
nannisa727@gmail.com <nannisa727@gmail.com>
2021-10-01 22:41:49 +0700

    Upload awal project

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Annisa</li>
+        <li>Nama Belakang : Nurul Madinah</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Oevang Oeray Blok C.04</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Sintang</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Renang</li>
+    </ul>  
+</section>
